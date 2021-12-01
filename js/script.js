b63ceb9f185d93265045edfb41aec59ab957c2e3
keloholic <72310838+keloholic@users.noreply.github.com>
2021-12-01 12:50:06 -0600

    Started on the project
    started the initial builds of the first function

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 /*
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
@@ -12,13 +13,42 @@ For assistance:
 */
 
 
+const itemsPerPage = 2;
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const showPage = (list, page) => {
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
+//Selects the UL element with a class of student list and assins value it studentList
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
 
+//Loop over list parameter
+   for(let i = 0; i < list.length; i++) {
+      if (i >= startIndex && i < endIndex) {
+   
+        studentList.insertAdjacentHTML =`<li class="student-item cf">
+          <div class="student-details">
+            <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+            <h3>Ethel Dean</h3>
+            <span class="email">ethel.dean@example.com</span>
+          </div>
+          <div class="joined-details">
+            <span class="date">Joined 12-15-2005</span>
+          </div>
+        </li>`
 
+      };
+   }
+
+};
+
+const liste = ['yes','no','happy','sad'];
+
+showPage(liste, itemsPerPage);
 
 /*
 Create the `addPagination` function
